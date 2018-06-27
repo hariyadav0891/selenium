f23e76cefd00a2f466a0c38d8538f494edbabc19
Hari shanker <33596401+hariyadav0891@users.noreply.github.com>
2018-06-27 22:58:19 +0530

    Add files via upload

---
+++ TestBase1.java
@@ -0,0 +1,110 @@
+package demo.selenium;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class TestBase1 {
+	public static final Logger log = Logger.getLogger(TestBase.class.getName());
+	public static ExtentReports extent;
+	public static ExtentTest test;
+	public ITestResult result;
+	public static WebDriver driver;
+	public void log(String data) {
+		log.info(data);
+		Reporter.log(data);
+		test.log(LogStatus.INFO, data);
+	}
+
+	@BeforeTest
+	public void setExtent(){
+		extent = new ExtentReports(System.getProperty("user.dir")+"/Extend_Report/ExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Hari Shanker Window");
+		extent.addSystemInfo("User Name", "Hari shanker QA");
+		extent.addSystemInfo("Environment", "QA");
+		
+	}
+
+	@AfterMethod()
+	public void afterMethod(ITestResult result) {
+		if (result.getStatus() == ITestResult.SUCCESS) {
+			test.log(LogStatus.PASS, result.getName() + " test is pass");
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			test.log(LogStatus.SKIP, result.getName() + " test is skipped and skip reason is:-" + result.getThrowable());
+		} else if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(LogStatus.ERROR, result.getName() + " test is failed" + result.getThrowable());
+			String screen = captureScreen("");
+			test.log(LogStatus.FAIL, test.addScreenCapture(screen));
+		} else if (result.getStatus() == ITestResult.STARTED) {
+			test.log(LogStatus.INFO, result.getName() + " test is started");
+		}
+	}
+
+	@BeforeMethod()
+	public void beforeMethod(Method result) {
+		test = extent.startTest(result.getName());
+		test.log(LogStatus.INFO, result.getName() + " test Started");
+		System.setProperty("webdriver.chrome.driver", "E:\\testing\\lib\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "E:/teting/lib/Drivers/chromedriver_win32/chromedriver.exe");	
+		driver = new ChromeDriver(); 
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.freecrm.com/");
+		
+		
+	}
+
+	@AfterTest
+	public void endReport(){
+		extent.flush();
+		extent.close();
+		driver.quit();
+	}
+
+	public String captureScreen(String fileName) {
+		if (fileName == "") {
+			fileName = "blank";
+		}
+		File destFile = null;
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+		try {
+			String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/src/main/screenshot/";
+			destFile = new File((String) reportDirectory + fileName + "_" + formater.format(calendar.getTime()) + ".png");
+			FileUtils.copyFile(scrFile, destFile);
+			// This will help us to link the screen shot in testNG report
+			Reporter.log("<a href='" + destFile.getAbsolutePath() + "'> <img src='" + destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return destFile.toString();
+	}
+	
+
+}
